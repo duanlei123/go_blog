b6371e9dd25a01445a920ac09ba9b8743b736512
wb-dl321273 <wb-dl321273@autonavi.com>
2019-11-27 17:14:43 +0800

    国际化

--- controllers/default.go
+++ controllers/default.go
@@ -2,11 +2,27 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/beego/i18n"
 	"go_blog/models"
 )
 
-type MainController struct {
+type baseController struct {
 	beego.Controller
+	i18n.Locale
+}
+
+func (this *baseController) Prepare(){
+	lang := this.GetString("lang")
+	if lang == "zh-CN"{
+		this.Lang = lang
+	}else {
+		this.Lang = "en-US"
+	}
+	this.Data["Lang"] = this.Lang
+}
+
+type MainController struct {
+	baseController
 }
 
 func (c *MainController) Get() {
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/beego/i18n"
 	"go_blog/models"
 	_ "go_blog/routers"
 	"github.com/astaxie/beego"
@@ -16,6 +17,12 @@ func main() {
 	beego.SetStaticPath("/static","static")
 	orm.Debug = true
 	_ = orm.RunSyncdb("default", false, true)
+	//注册国际化
+	i18n.SetMessage("en-US", "conf/locale_en-US.ini")
+	i18n.SetMessage("zh-CN", "conf/locale_zh-CN.ini")
+	// 注册模板函数
+	beego.AddFuncMap("i18n", i18n.Tr)
+
 	beego.Run()
 }
 
